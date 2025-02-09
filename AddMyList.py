afc20d4af3ba11720547412d24fa5916544e9593
Sayan Naha <82594248+sayannaha@users.noreply.github.com>
2025-02-09 20:49:22 +0530

    Python Code to Add Odd and Even Elements from 2 different Lists consecutively

---
+++ AddMyList.py
@@ -0,0 +1,14 @@
+#Create a list by picking an odd-index items from the first list and even index items from the second
+list1 = [3, 6, 9, 12, 15, 18, 21]
+list2 = [4, 8, 12, 16, 20, 24, 28]
+res = list()
+odd_elements = list1[1::2]
+print("Element at odd-index positions from list one")
+print(odd_elements)
+even_elements = list2[0::2]
+print("Element at even-index positions from list two")
+print(even_elements)
+print("Printing Final third list")
+res.extend(odd_elements)
+res.extend(even_elements)
+print(res)
